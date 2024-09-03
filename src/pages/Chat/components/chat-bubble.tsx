245fb29e6c31b091c7aa1b7b22759953ae07d7c9
Mark Hegedus <mark@qualisync.com>
2024-09-03 20:17:25 +0200

    add chat bubble with mock text

---
+++ src/pages/Chat/components/chat-bubble.tsx
@@ -0,0 +1,56 @@
+import { Avatar, Flex, Space, theme, Typography } from "antd";
+import type { CSSProperties } from "react";
+
+const { Text } = Typography;
+
+type Props = {
+  content: string;
+  isMe: boolean;
+  avatar: string;
+};
+
+const { useToken } = theme;
+
+const ChatBubble = ({ content, isMe, avatar }: Props) => {
+  const { token } = useToken();
+
+  const bubbleStyle: CSSProperties = {
+    backgroundColor: isMe ? token.colorPrimaryBg : token.colorFillSecondary,
+    // color: isMe ? "white" : "black",
+    padding: "8px 12px",
+    borderRadius: "12px",
+    maxWidth: "70%",
+    alignSelf: isMe ? "flex-end" : "flex-start",
+  };
+
+  const containerStyle: CSSProperties = {
+    display: "flex",
+    flexDirection: isMe ? "row-reverse" : "row",
+    alignItems: "flex-start",
+    marginBottom: "16px",
+  };
+
+  return (
+    <div style={containerStyle}>
+      <Avatar
+        src={avatar}
+        alt={isMe ? "User Avatar" : "Other Avatar"}
+        style={{ marginRight: isMe ? 0 : 8, marginLeft: isMe ? 8 : 0 }}
+      />
+      <Flex
+        // direction="vertical"
+        align={isMe ? "end" : "start"}
+        // size={2}
+        style={{ maxWidth: "100%" }}
+        vertical
+      >
+        <Text strong>{isMe ? "You" : "Other"}</Text>
+        <div style={bubbleStyle}>
+          <Text>{content}</Text>
+        </div>
+      </Flex>
+    </div>
+  );
+};
+
+export default ChatBubble;
--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,11 +1,23 @@
 import { Button, Input, Space } from "antd";
 import SendChatMessage from "./components/send-chat-message";
+import ChatBubble from "./components/chat-bubble";
 
 type Props = {};
 
 const Chat = (props: Props) => {
   return (
     <>
+      <ChatBubble
+        content="Hello, how are you?"
+        isMe={false}
+        avatar="https://example.com/other-avatar.png"
+      />
+      <ChatBubble
+        content="I'm doing great, thanks for asking!"
+        isMe={true}
+        avatar="https://example.com/user-avatar.png"
+      />
+
       <SendChatMessage />
     </>
   );
