518379bd96d94746ed115491d317854fc50f44f3
Mark Hegedus <Mark.Hegedus3@tesco.com>
2024-09-05 11:05:55 +0200

    add bug note

--- src/pages/Chat/use-chat.ts
+++ src/pages/Chat/use-chat.ts
@@ -5,6 +5,12 @@ const useChat = () => {
   const chatHistory = useChatHistory((state) => state.chatHistory);
   useEffect(() => {
     window.scrollTo(0, document.body.scrollHeight);
+
+    //
+    // BUG: depending on chatHistory will create an interesting bug,
+    // that when someone else posts a new chat it will also scroll down to the bottom
+    // it would be a nice to have to have a scroll to newest upon not current user Message, but
+    // scroll to bottom if curent user posts a message
   }, [chatHistory]);
 
   return chatHistory;
