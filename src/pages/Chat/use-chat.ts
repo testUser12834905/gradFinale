d78a5b5f11c039135f772eeb35088b2fc19b9b2f
Mark Hegedus <Mark.Hegedus3@tesco.com>
2024-09-05 10:35:26 +0200

    add scroll to bottom on initial render and upon new message received

--- src/pages/Chat/use-chat.ts
+++ src/pages/Chat/use-chat.ts
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import { useChatHistory } from "../../lib/state/chat-history";
 
 const useChat = () => {
   const chatHistory = useChatHistory((state) => state.chatHistory);
+  useEffect(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, [chatHistory]);
 
   return chatHistory;
 };
