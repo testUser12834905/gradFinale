8956e64b4600c96663be6a6862e02ab682ce7e64
Mark Hegedus <Mark.Hegedus3@tesco.com>
2024-09-06 11:04:49 +0200

    remove useless parts

--- src/hooks/useMessageServer.ts
+++ src/hooks/useMessageServer.ts
@@ -6,37 +6,6 @@ import {
 import { useWebSocket } from "../lib/state/web-socket";
 import type { MessageInstance } from "antd/es/message/interface";
 
-const createWebSocket = (
-  url: string,
-  dataActions: ChatHistoryAction["chatHistoryActions"],
-) => {
-  const newSocket = new WebSocket(url);
-
-  newSocket.onopen = () => {
-    console.log("WebSocket connected");
-  };
-
-  newSocket.onmessage = (event) => {
-    const ch = JSON.parse(event.data);
-    dataActions.initialize(ch);
-    console.log("Message from server:", event);
-  };
-
-  newSocket.onerror = (error) => {
-    console.error("WebSocket error:", error);
-  };
-
-  newSocket.onclose = (event) => {
-    if (event.wasClean) {
-      console.log(`Closed cleanly, code=${event.code}, reason=${event.reason}`);
-    } else {
-      console.error("Connection died");
-    }
-  };
-
-  return newSocket;
-};
-
 // TODO: rename this to something more clear
 const useMessageServer = (messageApi: MessageInstance) => {
   const chatHistoryActions = useChatHistory(
@@ -48,7 +17,6 @@ const useMessageServer = (messageApi: MessageInstance) => {
   const closeWebSocket = useWebSocket((state) => state.closeWebSocket);
 
   const webSocketUrl = "ws://localhost:8080";
-  console.log("wtf: ", webSocketUrl);
 
   const initializeWebSocket = useCallback(() => {
     if (!webSocket) {
